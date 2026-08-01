db5e1695a08382efd3872c49b2a3dc08d99a7ce9
Bharat Kumar Sharma <55586537+iambharatks@users.noreply.github.com>
2026-08-01 19:03:43 +0530

    leetcode: 0486-predict-the-winner solved with Medium level on 08-01-2026

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -14,6 +14,12 @@ public:
         int n = size(nums);
         dp.assign(n,vector<int>(n,-1));
         int res = choose(nums,0,size(nums)-1);
-        return res >= 0;
+        for(int i = 0 ; i < n ; i++) dp[i][i] = nums[i];
+        for(int l = n-1; l >=0 ; l--){
+            for(int r = l+1 ; r < n ; r++){
+                dp[l][r] = max(nums[l]-dp[l+1][r],nums[r]-dp[l][r-1]);
+            }
+        }
+        return dp[0][n-1] >= 0;
     }
 };
