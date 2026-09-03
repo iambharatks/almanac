e69271db05eb2cfbe20aa33b48e62b63df6f766c
Bharat Kumar Sharma <55586537+iambharatks@users.noreply.github.com>
2026-09-03 23:38:09 +0530

    leetcode: 3876-construct-uniform-parity-array-ii solved with Medium level on 09-03-2026

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean uniformArray(int[] nums) {
+        // int even = nums.length;
+        // int odd =0;
+        int minE = Integer.MAX_VALUE;
+        int minO = Integer.MAX_VALUE;
+        for(int i : nums){
+            if(i%2==1) {
+                // even--;odd++;
+                minO = Math.min(minO,i);
+            }else
+                minE = Math.min(minE,i);
+        }
+        if(minO == Integer.MAX_VALUE || minE == Integer.MAX_VALUE) return true;
+        // if(even == 0 || odd == 0) return true;
+        // int min = Math.min(minO,minE);
+        // for(int i : nums){
+        //     if(i > min){
+        //         if(i%2 == min%2) continue;
+        //         if(i%2 == 0) continue;
+        //         if(i > minO) continue;
+        //         return false;
+        //     }
+        // }
+        return minO < minE;
+    }
+}
